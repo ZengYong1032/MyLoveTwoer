8b50219334a7c81ef9d75e5a3333a86b811f90cb
ZyZl <zy@ZyZldeMacBook-Air.local>
2017-08-30 09:49:01 +0800

    Initial Commit

---
+++ MyLoveCastle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyLoveCastle
+//
+//  Created by ZyZl on 2017/8/30.
+//  Copyright © 2017年 ZyZl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyLoveCastle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyLoveCastle
+//
+//  Created by ZyZl on 2017/8/30.
+//  Copyright © 2017年 ZyZl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
